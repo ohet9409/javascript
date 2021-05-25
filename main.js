95dfd274300dbff80ba4ae11cf24c3175b802cb4
ohet9409 <dhdmsxor1@naver.com>
2021-05-25 22:30:19 +0900

    비교 연산자, 논리 연산자

--- main.js
+++ main.js
@@ -22,18 +22,53 @@
 // console.log(getType([]))
 
 // 산술 연산자(arithmetic operator)
-console.log(1 + 2)
-console.log(5 - 2)
-console.log(9 * 2)
-console.log(10 / 2)
-console.log(9 % 2)
+// console.log(1 + 2) // 더하기
+// console.log(5 - 2) // 빼기
+// console.log(9 * 2) // 곱하기
+// console.log(10 / 2) // 나누기
+// console.log(9 % 2) // 나머지
 
-// 할당 연산자(aaignment operator)
-// const: 재할당 불가
-const a = 2
-console.log(a)
-// let: 재할당 가능
-let b = 1
-// b = b + 4
-b += 4
-console.log(b)
+// // 할당 연산자(aaignment operator)
+// // const: 재할당 불가
+// const a = 2
+// console.log(a)
+// // let: 재할당 가능
+// let b = 1
+// // b = b + 4
+// b += 4
+// console.log(b)
+
+// 비교 연산자(comparison operator)
+const a = 1
+const b = 3
+//  일치 연산자
+console.log(a === b)
+// 일치 연산자를 이용한 함수 생성
+function isEqual(x, y){
+    return x === y
+}
+console.log(isEqual(1,1))
+// 데이터 형이 다르기 때문에 false 출력
+console.log(isEqual("1",1))
+// 서로 다른 값인지 확인하는 연산자
+console.log(a !== b)
+// a가 b보다 큰지 확인하는 연산자
+console.log(a > b)
+// = 기호를 반드시 뒤쪽에 위치 시켜야 한다
+console.log(a >= b)
+
+// 논리 연산자
+const a1 = 1 === 1
+const b1 = 'AB' === 'AB'
+const c1 = false
+
+console.log(a1)
+console.log(b1)
+console.log(c1)
+
+// and 연산자: 모두 true 일때 true 반환
+console.log('&&: ', a1 && b1 && c1 )
+// or 연산자: 한가지 값이 true이면 true 반환
+console.log('||: ', a1 || b1 || c1 )
+// not 연산자: 반대값을 출력
+console.log('!: ', !a1 )
