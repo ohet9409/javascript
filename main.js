beae932197851621b88de3ede72fc7f2b4219051
ohet9409 <dhdmsxor1@naver.com>
2021-05-25 22:39:43 +0900

    삼항 연산자

--- main.js
+++ main.js
@@ -39,36 +39,47 @@
 // console.log(b)
 
 // 비교 연산자(comparison operator)
-const a = 1
-const b = 3
-//  일치 연산자
-console.log(a === b)
-// 일치 연산자를 이용한 함수 생성
-function isEqual(x, y){
-    return x === y
+// const a = 1
+// const b = 3
+// //  일치 연산자
+// console.log(a === b)
+// // 일치 연산자를 이용한 함수 생성
+// function isEqual(x, y){
+//     return x === y
+// }
+// console.log(isEqual(1,1))
+// // 데이터 형이 다르기 때문에 false 출력
+// console.log(isEqual("1",1))
+// // 서로 다른 값인지 확인하는 연산자
+// console.log(a !== b)
+// // a가 b보다 큰지 확인하는 연산자
+// console.log(a > b)
+// // = 기호를 반드시 뒤쪽에 위치 시켜야 한다
+// console.log(a >= b)
+
+// // 논리 연산자
+// const a1 = 1 === 1
+// const b1 = 'AB' === 'AB'
+// const c1 = false
+
+// console.log(a1)
+// console.log(b1)
+// console.log(c1)
+
+// // and 연산자: 모두 true 일때 true 반환
+// console.log('&&: ', a1 && b1 && c1 )
+// // or 연산자: 한가지 값이 true이면 true 반환
+// console.log('||: ', a1 || b1 || c1 )
+// // not 연산자: 반대값을 출력
+// console.log('!: ', !a1 )
+
+// 삼항 연산자(ternary operator)
+const a = 1 < 2
+
+if (a) {
+    console.log('참')
+} else {
+    console.log('거짓')
 }
-console.log(isEqual(1,1))
-// 데이터 형이 다르기 때문에 false 출력
-console.log(isEqual("1",1))
-// 서로 다른 값인지 확인하는 연산자
-console.log(a !== b)
-// a가 b보다 큰지 확인하는 연산자
-console.log(a > b)
-// = 기호를 반드시 뒤쪽에 위치 시켜야 한다
-console.log(a >= b)
-
-// 논리 연산자
-const a1 = 1 === 1
-const b1 = 'AB' === 'AB'
-const c1 = false
-
-console.log(a1)
-console.log(b1)
-console.log(c1)
-
-// and 연산자: 모두 true 일때 true 반환
-console.log('&&: ', a1 && b1 && c1 )
-// or 연산자: 한가지 값이 true이면 true 반환
-console.log('||: ', a1 || b1 || c1 )
-// not 연산자: 반대값을 출력
-console.log('!: ', !a1 )
+// true이면 첫번째, false이면 두번째 값 출력
+console.log(a ? '참' : '거짓')
